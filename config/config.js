845e082083de5c1fc9d82d3888d615446bb2bb50
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-11-30 23:18:53 +1100

    server update

---
+++ config/config.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+const config = {
+  salt: +process.env.SALT || 10,
+  port: process.env.PORT || 5050,
+  mongoOptions: {
+    url: process.env.MONGODB_URL || "mongodb://localhost:27017/health-care",
+  },
+
+  jwt: {
+    secret: process.env.JWT_SECRET || "secret",
+    expiresin: process.env.EXPIRES_IN || "7d",
+    refresh_secret: process.env.JWT_REFRESH_SECRET || "REFRESH-SECRET_KEY",
+    refresh_expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "30d",
+  },
+
+  frontend: {
+    domain: process.env.ROOT_DOMAIN,
+  },
+};
+
+export default config;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { mongooseConnect } from "./config/mongoConfig";
+import config from "./config/config.js";
+import { mongooseConnect } from "./config/mongoConfig.js";
 
 const app = express();
 app.use(cors());
@@ -10,9 +11,23 @@ app.get("/", (req, res) => {
   res.send("API endpoint for Healthcare");
 });
 
+// api error handler
+app.all("/^/.*$/", (req, res, next) => {
+  next(createApiError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({
+    status: "error",
+    message: err.message || "Internal Server Error",
+  });
+});
+
 mongooseConnect()
   .then(() => {
-    server.listen(config.port, () => {
+    app.listen(config.port, () => {
       console.log(`✅ Server started on port ${config.port}`);
     });
   })
